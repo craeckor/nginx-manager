1109fac3bd9a911aed474a09999e0ad6ea269d60
craeckor <52975262+craeckor@users.noreply.github.com>
2024-11-18 10:08:58 +0000

    Update ps2exe.ps1

--- ps2exe.ps1
+++ ps2exe.ps1
@@ -1,13 +1,21 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe -ArgumentList "-NoProfile", "-ExecutionPolicy Bypass", "-Command", "cd `"$($PWD.Path)`"; .\ps2exe.ps1" -Verb RunAs; exit }
 
-$nuget = Get-PackageProvider -Name "NuGet"
+$workpath = $PSScriptRoot
+
+Set-Location -Path $workpath
+
+# Do not prompt user for confirmations
+Set-Variable -Name 'ConfirmPreference' -Value 'None' -Scope Global
+
+Install-PackageProvider NuGet -Scope AllUsers -ForceBootstrap -Confirm:$false -Force
+$nuget = Get-PackageProvider -Name "NuGet" -ErrorAction SilentlyContinue
 
 if ($nuget) {
     $psgallery = (Get-PSRepository -Name "PSGallery").InstallationPolicy
     if ($psgallery -eq "Trusted") {
         $ps2exe = Get-Module -Name "ps2exe"
         if (!($ps2exe)) {
-            Install-Module ps2exe -Repository PSGallery
+            Install-Module ps2exe -Repository PSGallery -Scope AllUsers -SkipPublisherCheck -Force
             Import-Module -Name "ps2exe" -Force
         } else {
             Import-Module -Name "ps2exe" -Force
@@ -15,16 +23,16 @@ if ($nuget) {
     } else {
         Set-PSRepository PSGallery -InstallationPolicy Trusted
         if (!($ps2exe)) {
-            Install-Module ps2exe -Repository PSGallery
+            Install-Module ps2exe -Repository PSGallery -Scope AllUsers -SkipPublisherCheck -Force
             Import-Module -Name "ps2exe" -Force
         } else {
             Import-Module -Name "ps2exe" -Force
         }
     }
 } else {
-    Install-PackageProvider NuGet -Force
+    Install-PackageProvider NuGet -Scope AllUsers -ForceBootstrap -Confirm:$false -Force
     Set-PSRepository PSGallery -InstallationPolicy Trusted
-    Install-Module ps2exe -Repository PSGallery -Force
+    Install-Module ps2exe -Repository PSGallery -Force -Scope AllUsers -SkipPublisherCheck -Force
     Import-Module -Name "ps2exe" -Force
 }
 
